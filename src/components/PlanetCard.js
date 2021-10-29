cbc48d45aa904e068b7e48bf14af78c364f30eaa
Emerson <eemr3@yahoo.com.br>
2021-10-29 19:28:19 -0300

    fix(style): correção nomes dos planetas sem cor

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -6,7 +6,7 @@ class PlanetCard extends Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div data-testid="planet-card" className="content-card">
         <h3 data-testid="planet-name">{planetName}</h3>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
